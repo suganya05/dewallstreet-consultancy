1af9433b782e1c4ddd18181ed6c2e3a81ad50463
suganya05 <go2suganya@gmail.com>
2023-03-29 14:47:37 +0530

    chore: custom.d.ts file create

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import RollArrow from "../../assets/images/roll-arrow.png";
+import "./Home.scss";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="mx">
+      <div className="home-wrapper">
+        <div className="first-content">
+          <h3>We are the best professional service network for all your</h3>
+          <div>
+            <h1>A</h1>
+            <img src={RollArrow} alt="" />
+            <h1>Z</h1>
+          </div>
+        </div>
+        <div className="second-content"></div>
+      </div>
     </div>
   );
 };
