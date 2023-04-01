6832088b0207a7dc238f37dd58cac3f64db9e365
vigneshdfc <vigneshdfc02@gmail.com>
2023-04-01 16:44:30 +0530

    chore:pull

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -3,7 +3,7 @@ import logo from "../../assets/images/dewallstreet.svg";
 import "./root.scss";
 import { ReactComponent as RollArrow } from "../../assets/images/roll-arrow-white.svg";
 import LineOfBusinesses from "../../components/LineOfBusinesses";
-import Fotter from "../../components/Fotter";
+import Footer from "../../components/Footer";
 
 const Root: React.FC = () => {
   return (
@@ -17,35 +17,39 @@ const Root: React.FC = () => {
                 type="video/mp4"
               />
             </video>
-          </div>
-          <div className="dewall_logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="">
-            <h3>We are the best professional service network for all your</h3>
-          </div>
-          <div className="a-to-z-content">
-            <h1>A</h1>
-            <RollArrow />
-            <h1>Z</h1>
-          </div>
-          <h2>business needs.</h2>
-          <div>
-            <p>
-              Dewallstreet Consultancy is a legally structured unit of
-              Dewallstreet Private Limited, an India based multiple interest
-              conglomeration foundation in its early seed stage, which is
-              ethically incorporated and regulated under the norms of Ministry
-              of Corporate Affairs (MCA) & Registrar of Companies (RoC) and
-              follows industrial reputation trust marks, quality delivery
-              standards, ethical business & market practices with Go-Global
-              bootstrapped principle.
-            </p>
+            <div className="dewall_heropage">
+              <div className="dewall_logo">
+                <img src={logo} alt="logo" />
+              </div>
+              <div className="">
+                <h3>
+                  We are the best professional service network for all your
+                </h3>
+              </div>
+              <div className="a-to-z-content">
+                <h1>A</h1>
+                <RollArrow />
+                <h1>Z</h1>
+              </div>
+              <h2>business needs.</h2>
+              <div>
+                <p>
+                  Dewallstreet Consultancy is a legally structured unit of
+                  Dewallstreet Private Limited, an India based multiple interest
+                  conglomeration foundation in its early seed stage, which is
+                  ethically incorporated and regulated under the norms of
+                  Ministry of Corporate Affairs (MCA) & Registrar of Companies
+                  (RoC) and follows industrial reputation trust marks, quality
+                  delivery standards, ethical business & market practices with
+                  Go-Global bootstrapped principle.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <LineOfBusinesses />
-      <Fotter />
+      <Footer />
     </>
   );
 };
